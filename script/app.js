f289559c029af8cc640945b6e256b31f63e5e147
Md. Saadman Galib <73209315+saadman-galib@users.noreply.github.com>
2022-10-13 23:16:18 +0600

    Navbar Done

--- script/app.js
+++ script/app.js
@@ -13,3 +13,39 @@ function toggleAccordion() {
 }
 
 items.forEach((item) => item.addEventListener('click', toggleAccordion));
+
+// function toggleNav() {
+//   var x = document.getElementById("myTopnav");
+//   if (x.className === "topnav") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "topnav";
+//   }
+// }
+
+var navBtn = document.querySelector('.nav-btn')
+var navListContainer = document.querySelector('.nav-items')
+var contactContainer = document.querySelector('.contact')
+
+
+var a = 0
+
+navBtn.addEventListener("click", function(){
+    // if (a <=0){
+    //     navListContainer.style.left = '100vw'
+    //     a += 1 
+    // }
+    // else{
+    //     pass
+    // }
+    // console.log('Hi')
+    if (navListContainer.style.left == '0px'){
+      navListContainer.style.left = '-100vw';
+      contactContainer.style.left = '-100vw';
+    }
+
+    else{
+      navListContainer.style.left = "0px";
+      contactContainer.style.left = '0px';
+    }
+});
