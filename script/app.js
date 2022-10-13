405ad554c50d83762c20d21833bde7629ff0fa8c
Md. Saadman Galib <73209315+saadman-galib@users.noreply.github.com>
2022-10-13 23:24:06 +0600

    nav hide added

--- script/app.js
+++ script/app.js
@@ -1,51 +1,48 @@
-const items = document.querySelectorAll('.accordion button');
+const items = document.querySelectorAll(".accordion button");
+var navBtn = document.querySelector(".nav-btn");
+var navListContainer = document.querySelector(".nav-items");
+var contactContainer = document.querySelector(".contact");
+var aboutBtn = document.querySelector(".about-btn");
+var productBtn = document.querySelector(".product-btn");
+var faqBtn = document.querySelector(".faq-btn");
 
 function toggleAccordion() {
-  const itemToggle = this.getAttribute('aria-expanded');
+    const itemToggle = this.getAttribute("aria-expanded");
 
-  for (i = 0; i < items.length; i++) {
-    items[i].setAttribute('aria-expanded', 'false');
-  }
+    for (i = 0; i < items.length; i++) {
+        items[i].setAttribute("aria-expanded", "false");
+    }
 
-  if (itemToggle == 'false') {
-    this.setAttribute('aria-expanded', 'true');
-  }
+    if (itemToggle == "false") {
+        this.setAttribute("aria-expanded", "true");
+    }
 }
 
-items.forEach((item) => item.addEventListener('click', toggleAccordion));
+items.forEach((item) => item.addEventListener("click", toggleAccordion));
 
-// function toggleNav() {
-//   var x = document.getElementById("myTopnav");
-//   if (x.className === "topnav") {
-//     x.className += " responsive";
-//   } else {
-//     x.className = "topnav";
-//   }
-// }
+var a = 0;
 
-var navBtn = document.querySelector('.nav-btn')
-var navListContainer = document.querySelector('.nav-items')
-var contactContainer = document.querySelector('.contact')
-
-
-var a = 0
-
-navBtn.addEventListener("click", function(){
-    // if (a <=0){
-    //     navListContainer.style.left = '100vw'
-    //     a += 1 
-    // }
-    // else{
-    //     pass
-    // }
-    // console.log('Hi')
-    if (navListContainer.style.left == '0px'){
-      navListContainer.style.left = '-100vw';
-      contactContainer.style.left = '-100vw';
+navBtn.addEventListener("click", function () {
+    if (navListContainer.style.left == "0px") {
+        navListContainer.style.left = "-100vw";
+        contactContainer.style.left = "-100vw";
+    } else {
+        navListContainer.style.left = "0px";
+        contactContainer.style.left = "0px";
     }
+});
 
-    else{
-      navListContainer.style.left = "0px";
-      contactContainer.style.left = '0px';
-    }
+aboutBtn.addEventListener("click", () => {
+    navListContainer.style.left = "-100vw";
+    contactContainer.style.left = "-100vw";
+});
+
+productBtn.addEventListener("click", () => {
+    navListContainer.style.left = "-100vw";
+    contactContainer.style.left = "-100vw";
+});
+
+faqBtn.addEventListener("click", () => {
+    navListContainer.style.left = "-100vw";
+    contactContainer.style.left = "-100vw";
 });
